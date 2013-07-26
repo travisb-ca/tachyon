86a43fef9055419737e18860a2bd1dbaa34aacf0
Travis Brown <travisb@travisbrown.ca>
2013-07-26 05:12:26 +0000

    Add a tool which receives input on stdin and echos it to stdout two seconds later.
    git-svn-id: svn+ssh://svnrepo/projects/tachyon/trunk@6562 561c3618-0fe6-0310-b5d6-fc65c73fee30

---
+++ tools/delayed_echo.c
@@ -0,0 +1,85 @@
+/*
+ * A simple program which takes every character from stdin and echos it to stdout
+ * after a delay.
+ */
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <termios.h>
+
+#define DELAY_US (2000 * 1000)
+#define BUF_SIZE 4096
+
+#define STDIN 0
+#define STDOUT 1
+
+int main(int argn, char **args)
+{
+	int result;
+	struct timeval wait;
+	struct timeval now;
+	struct timeval *departure;
+	fd_set fds;
+	int used = 0;
+	struct termios termstate;
+
+	struct {
+		char c;
+		struct timeval departure;
+	} buf[BUF_SIZE];
+
+	tcgetattr(STDIN, &termstate);
+	termstate.c_lflag &= ~ICANON;
+	termstate.c_cc[VMIN] = 1;
+	tcsetattr(STDIN, TCSANOW, &termstate);
+
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+	for (;;) {
+		if (used == 0) {
+			wait.tv_sec = 10000000;
+			wait.tv_usec = 0;
+		} else {
+			gettimeofday(&now, NULL);
+			departure = &buf[0].departure;
+			
+			wait.tv_sec = departure->tv_sec - now.tv_sec;
+			wait.tv_usec = departure->tv_usec - now.tv_usec;
+
+			if (wait.tv_usec < 0) {
+				wait.tv_sec -= 1;
+				wait.tv_usec += 1000000;
+			}
+		}
+
+		FD_ZERO(&fds);
+		FD_SET(STDIN, &fds);
+		result = select(STDIN + 1, &fds, NULL, NULL, &wait);
+
+		if (result == 0) {
+			/* select timed out, output this character */
+			putc(buf[0].c, stdout);
+			used--;
+			memmove(buf, buf + 1, sizeof(buf[0]) * used);
+		} else {
+			/* Have a new character */
+			if (used == BUF_SIZE)
+				continue; /* Drop it */
+
+			buf[used].c = getc(stdin);
+			departure = &buf[used].departure;
+			gettimeofday(departure, NULL);
+			departure->tv_usec += DELAY_US;
+			while (departure->tv_usec > 1000000) {
+				departure->tv_usec -= 1000000;
+				departure->tv_sec += 1;
+			}
+			used++;
+		}
+	}
+
+	return 0;
+}
