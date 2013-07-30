a6b4f90090bf8a54921d5e82ff71a6c7dafdfa13
Travis Brown <travisb@travisbrown.ca>
2013-07-30 03:07:02 +0000

    Convert tty.c to the new logging macros.
    git-svn-id: svn+ssh://svnrepo/projects/tachyon/trunk@6579 561c3618-0fe6-0310-b5d6-fc65c73fee30

--- src/tty.c
+++ src/tty.c
@@ -29,6 +29,8 @@
 #include <sys/select.h>
 #include <sys/ioctl.h>
 
+#include "log.h"
+
 /*
  * Create a new slave tty running the given command.
  *
@@ -45,28 +47,28 @@ int tty_new(char *command)
 	pty_master = posix_openpt(O_RDWR | O_NOCTTY);
 
 	if (pty_master < 0) {
-		fprintf(stderr, "openpt failed %d %d\n", pty_master, errno);
+		ELOG("openpt failed %d %d\n", pty_master, errno);
 		result = -1;
 		goto err_master;
 	}
 
 	result = grantpt(pty_master);
 	if (result != 0) {
-		fprintf(stderr, "granpt failed %d %d\n", result, errno);
+		ELOG("granpt failed %d %d\n", result, errno);
 		result = -2;
 		goto err_master;
 	}
 
 	result = unlockpt(pty_master);
 	if (result != 0) {
-		fprintf(stderr, "unlockpt failed %d %d\n", result, errno);
+		ELOG("unlockpt failed %d %d\n", result, errno);
 		result = -3;
 		goto err_master;
 	}
 
 	pty_slave = open(ptsname(pty_master), O_RDWR);
 	if (pty_slave < 0) {
-		fprintf(stderr, "failed to open slave %d %d\n", pty_slave, errno);
+		ELOG("failed to open slave %d %d\n", pty_slave, errno);
 		result = -4;
 		goto err_slave;
 	}
@@ -83,14 +85,14 @@ int tty_new(char *command)
 
 		result = tcgetattr(pty_slave, &term_settings);
 		if (result != 0) {
-			fprintf(stderr, "slave failed to get term_settings %d %d\n", result, errno);
+			ELOG("slave failed to get term_settings %d %d\n", result, errno);
 			return 5;
 		}
 
 		cfmakeraw(&term_settings);
 		result = tcsetattr(pty_slave, TCSANOW, &term_settings);
 		if (result != 0) {
-			fprintf(stderr, "slave failed to change to raw %d %d\n", result, errno);
+			ELOG("slave failed to change to raw %d %d\n", result, errno);
 			return 6;
 		}
 
@@ -108,14 +110,14 @@ int tty_new(char *command)
 
 		result = ioctl(0, TIOCSCTTY, 1);
 		if (result != 0) {
-			fprintf(stderr, "slave failed to set controlling tty %d %d\n", result, errno);
+			ELOG("slave failed to set controlling tty %d %d\n", result, errno);
 			return 7;
 		}
 
 
 		result = execv(command, NULL);
 
-		fprintf(stderr, "slave failed to exec %d %d\n", result, errno);
+		ELOG("slave failed to exec %d %d\n", result, errno);
 		return 8;
 	}
 
