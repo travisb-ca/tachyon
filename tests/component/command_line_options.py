0ac0d0f17686239d732e4ddab08ca5f1d933ab37
Travis Brown <travisb@travisbrown.ca>
2013-11-17 19:08:33 +0000

    Convert the single test to use the Python unittest module.
    git-svn-id: svn+ssh://svnrepo/projects/tachyon/branches/add_tests@6875 561c3618-0fe6-0310-b5d6-fc65c73fee30

--- tests/component/command_line_options.py
+++ tests/component/command_line_options.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python2.7
 
+import unittest
 import subprocess
-import time
 
-process = subprocess.Popen('../../tachyon -z', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-time.sleep(1)
-if process.poll() and process.returncode == 1:
-	print 'pass'
-else:
-	print 'fail'
+class TestCommandLineOptions(unittest.TestCase):
+	def setUp(self):
+		self.tachyon = None
+		self.tachyonTerminated = False
 
+	def tearDown(self):
+		if self.tachyon and not self.tachyonTerminated:
+			self.tachyon.kill()
+
+	def waitForTermination(self):
+		while not self.tachyon.poll():
+			pass
+		self.tachyonTerminated = True
+
+	def test_invalidOption(self):
+		self.tachyon = subprocess.Popen('./tachyon -z', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+		self.waitForTermination()
+
+		self.assertEqual(self.tachyon.returncode, 1)
+
+if __name__ == '__main__':
+	unittest.main()
