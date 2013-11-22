2772094197975ed2612609ca168035c05f91cecb
Travis Brown <travisb@travisbrown.ca>
2013-11-22 04:20:04 +0000

    Use the new Lousy process capabilities.
    git-svn-id: svn+ssh://svnrepo/projects/tachyon/trunk@6935 561c3618-0fe6-0310-b5d6-fc65c73fee30

--- tests/tachyon.py
+++ tests/tachyon.py
@@ -9,25 +9,19 @@ class TachyonTestCase(lousy.TestCase):
 
 	def setUp(self):
 		self.tachyon = None
-		self.tachyonTerminated = False
 
 	def tearDown(self):
-		if self.tachyon and not self.tachyonTerminated:
-			self.tachyon.kill()
+		if self.tachyon:
+			self.tachyon.terminate()
 
 	# Start a tachyon process with the given arguments
 	def startTachyon(self, args):
-		self.tachyon = subprocess.Popen('./tachyon ' + args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		self.tachyonTerminated = False
+		self.tachyon = lousy.Process('./tachyon ' + args, shell=True)
 
 	# Wait for the tachyon process to terminate. Fail if the timeout is exceeded
 	def waitForTermination(self, timeout=5):
-		startTime = time.time()
-		while not self.tachyon.poll():
-			if time.time() - startTime > timeout:
-				self.fail('Timed out waiting to process to terminate')
-			time.sleep(0.001)
-		self.tachyonTerminated = True
+		response = self.tachyon.waitForTermination(timeout)
+		self.assertTrue(response, 'Timed out waiting to process to terminate')
 
 	# Send the given string to tachyon as given
 	def sendString(self, string):
