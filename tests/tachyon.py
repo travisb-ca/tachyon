f8d1400ec865bd4a9fa231a6ec1518d1f37216f3
Travis Brown <travisb@travisbrown.ca>
2013-12-20 03:28:42 +0000

    Reshow the prompt after synchronizing to allow the other tests to be run.
    git-svn-id: svn+ssh://svnrepo/projects/tachyon/trunk@7140 561c3618-0fe6-0310-b5d6-fc65c73fee30

--- tests/tachyon.py
+++ tests/tachyon.py
@@ -23,6 +23,7 @@ class TachyonTestCase(lousy.TestCase):
 		self.tachyon = lousy.Process(cmd, shell=True, pty='vt100')
 		if sync:
 			self.expectPrompt('^bash.*\$ ')
+			self.sendLine('') # Reshow prompt
 
 	# Wait for the tachyon process to terminate. Fail if the timeout is exceeded
 	def waitForTermination(self, timeout=5):
