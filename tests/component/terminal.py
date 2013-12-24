8dd1fccedbafe3f6924d17638b6064b1532cff03
Travis Brown <travisb@travisbrown.ca>
2013-12-24 05:16:49 +0000

    Add a test that the terminal refreshes correctly when changing between buffers.
    git-svn-id: svn+ssh://svnrepo/projects/tachyon/trunk@7165 561c3618-0fe6-0310-b5d6-fc65c73fee30

---
+++ tests/component/terminal.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2.7
+
+import tachyon
+
+class TestTerminal(tachyon.TachyonTestCase):
+	def setUp1(self):
+		self.startTachyon()
+
+	def tearDown1(self):
+		self.waitForTermination()
+
+	def test_bufferContentsRestoredOnSwitch(self):
+		self.sendCmd('ls')
+		before = self.snapShot()
+
+		self.bufferCreate()
+		self.sendCmd('printenv')
+
+		self.bufferNext()
+
+		after = self.snapShot()
+		self.assertEqual(before, after)
