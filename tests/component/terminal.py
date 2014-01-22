3141fcd84778974439e616f9d34f7eeeb428f24c
Travis Brown <travisb@travisbrown.ca>
2014-01-22 05:48:08 +0000

    Refactor terminal tests some.
    git-svn-id: svn+ssh://svnrepo/projects/tachyon/trunk@7293 561c3618-0fe6-0310-b5d6-fc65c73fee30

--- tests/component/terminal.py
+++ tests/component/terminal.py
@@ -56,6 +56,20 @@ class TestTerminalEscapeCodes(tachyon.TachyonTestCase):
 		self.sendCmdExit()
 		self.waitForTermination()
 
+	def assertSurvivesBufferChange(self):
+		a = self.snapShot()
+		a_row, a_col = self.vtyCursorPosition()
+
+		self.bufferNext()
+		self.bufferNext()
+
+		b = self.snapShot()
+		b_row, b_col = self.vtyCursorPosition()
+
+		self.assertEqual(a, b)
+		self.assertEqual(a_row, b_row)
+		self.assertEqual(a_col, b_col)
+
 	def sendCsi(self, string):
 		self.pipe.write('\033[' + string)
 
@@ -86,25 +100,13 @@ class TestTerminalEscapeCodes(tachyon.TachyonTestCase):
 
 		self.sendCsi('J')
 
-		a = self.snapShot()
-
 		self.assertVtyString(0, 0, 'asdf')
 		for col in range(4, 20):
 			self.assertVtyCharIs(0, col, '')
 		for col in range(20):
 			self.assertVtyCharIs(1, col, '')
 		
-		row, col = self.tachyon.vty.cursorPosition()
-
-		self.bufferNext()
-		self.bufferNext()
-
-		b = self.snapShot()
-		self.assertEqual(a, b)
-
-		row, col = self.vtyCursorPosition()
-		self.assertEqual(row, 0)
-		self.assertEqual(col, 4)
+		self.assertSurvivesBufferChange()
 
 	def test_csiClearScreen_toEnd(self):
 		self.setCursorPos(0, 0)
@@ -116,25 +118,13 @@ class TestTerminalEscapeCodes(tachyon.TachyonTestCase):
 
 		self.sendCsi('0J')
 
-		a = self.snapShot()
-
 		self.assertVtyString(0, 0, 'asdf')
 		for col in range(4, 20):
 			self.assertVtyCharIs(0, col, '')
 		for col in range(20):
 			self.assertVtyCharIs(1, col, '')
-		
-		row, col = self.tachyon.vty.cursorPosition()
 
-		self.bufferNext()
-		self.bufferNext()
-
-		b = self.snapShot()
-		self.assertEqual(a, b)
-
-		row, col = self.vtyCursorPosition()
-		self.assertEqual(row, 0)
-		self.assertEqual(col, 4)
+		self.assertSurvivesBufferChange()
 
 	def test_csiClearScreen_fromStart(self):
 		self.setCursorPos(0, 0)
@@ -146,25 +136,13 @@ class TestTerminalEscapeCodes(tachyon.TachyonTestCase):
 
 		self.sendCsi('1J')
 
-		a = self.snapShot()
-
 		for col in range(20):
 			self.assertVtyCharIs(0, col, '')
 		for col in range(6):
 			self.assertVtyCharIs(1, col, '')
 		self.assertVtyString(1, 7, 'rqwer')
 		
-		row, col = self.vtyCursorPosition()
-
-		self.bufferNext()
-		self.bufferNext()
-
-		b = self.snapShot()
-		self.assertEqual(a, b)
-
-		row, col = self.vtyCursorPosition()
-		self.assertEqual(row, 1)
-		self.assertEqual(col, 6)
+		self.assertSurvivesBufferChange()
 
 	def test_csiClearScreen_all(self):
 		a = self.snapShot()
@@ -177,19 +155,11 @@ class TestTerminalEscapeCodes(tachyon.TachyonTestCase):
 		b = self.snapShot()
 		self.assertEqual(a, b)
 
-		row, col = self.tachyon.vty.cursorPosition()
+		row, col = self.vtyCursorPosition()
 		self.assertEqual(row, 23)
 		self.assertEqual(col, 0)
 
-		self.bufferNext()
-		self.bufferNext()
-
-		b = self.snapShot()
-		self.assertEqual(a, b)
-
-		row, col = self.tachyon.vty.cursorPosition()
-		self.assertEqual(row, 23)
-		self.assertEqual(col, 0)
+		self.assertSurvivesBufferChange()
 
 	def test_csiCursorPosition(self):
 		self.pipe.write('adfasdfasdfasdf\r\n;lkjh')
@@ -200,16 +170,14 @@ class TestTerminalEscapeCodes(tachyon.TachyonTestCase):
 
 		self.sendCsi('6;6f')
 
-		self.bufferNext()
-		self.bufferNext()
-
 		row, col = self.vtyCursorPosition()
 		self.assertEqual(row, 5)
 		self.assertEqual(col, 5)
 
 		self.pipe.write('z')
-
 		self.assertVtyCharIs(5, 5, 'z')
+
+		self.assertSurvivesBufferChange()
 
 	def test_csiCursorPosition_default(self):
 		self.pipe.write('adfasdfasdfasdf\r\n;lkjh')
@@ -220,16 +188,14 @@ class TestTerminalEscapeCodes(tachyon.TachyonTestCase):
 
 		self.sendCsi('f')
 
-		self.bufferNext()
-		self.bufferNext()
-
 		row, col = self.vtyCursorPosition()
 		self.assertEqual(row, 0)
 		self.assertEqual(col, 0)
 
 		self.pipe.write('z')
-
 		self.assertVtyCharIs(0, 0, 'z')
+
+		self.assertSurvivesBufferChange()
 
 	def test_csiCursorPosition_empty(self):
 		self.pipe.write('adfasdfasdfasdf\r\n;lkjh')
@@ -240,13 +206,11 @@ class TestTerminalEscapeCodes(tachyon.TachyonTestCase):
 
 		self.sendCsi(';f')
 
-		self.bufferNext()
-		self.bufferNext()
-
 		row, col = self.vtyCursorPosition()
 		self.assertEqual(row, 0)
 		self.assertEqual(col, 0)
 
 		self.pipe.write('z')
-
 		self.assertVtyCharIs(0, 0, 'z')
+		
+		self.assertSurvivesBufferChange()
